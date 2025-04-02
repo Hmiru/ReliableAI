5151f16dde579212cd8f62a777b3074817d689bf
Hmiru <mirunoyume@naver.com>
2025-04-02 09:43:53 +0000

    modified Eps value

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # 데이터셋 별 공격 시나리오
 mnist_attacks = [
-    dict(name="Targeted FGSM", sample_idx=1, k=1, eps=0.35, target=6, targeted=True),
+    dict(name="Targeted FGSM", sample_idx=1, k=1, eps=0.4, target=6, targeted=True),
     dict(name="Untargeted FGSM", sample_idx=14, k=1, eps=0.3, targeted=False),
     dict(name="Targeted PGD", sample_idx=1, k=10, eps=0.35, eps_step=0.025, target=6, targeted=True),
     dict(name="Untargeted PGD", sample_idx=14, k=10, eps=0.3, eps_step=0.025, targeted=False)
 ]
 
 cifar_attacks = [
-    dict(name="Targeted FGSM", sample_idx=1, k=1, eps=0.35, target=0, targeted=True),
+    dict(name="Targeted FGSM", sample_idx=1, k=1, eps=0.4, target=0, targeted=True),
     dict(name="Untargeted FGSM", sample_idx=14, k=1, eps=0.25, targeted=False),
     dict(name="Targeted PGD", sample_idx=1, k=10, eps=0.25, eps_step=0.025, target=0, targeted=True),
     dict(name="Untargeted PGD", sample_idx=14, k=10, eps=0.25, eps_step=0.025, targeted=False)
